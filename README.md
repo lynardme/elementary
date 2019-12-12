5fc7f8a45ffa11e202129102b3f77b207ac7bdbd
Lynard <lynardsalingujay@outlook.com>
2019-12-13 00:26:59 +1300

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # elementary
+
+[Ref](https://www.youtube.com/watch?v=buGFs1aQgaY)
---
+++ creational-patterns-python/factory.py
@@ -0,0 +1,26 @@
+class Dog:
+    """A simple dog class"""
+    def __init__(self, name):
+        self._name = name
+
+    def speak(self):
+        return "Awww"
+
+class Cat:
+    """A simple cat class"""
+    def __init__(self, name):
+        self._name = name
+
+    def speak(self):
+        return "Meow"
+
+def get_pet(pet="dog"):
+    """The factory method"""
+    pets = dict(dog=Dog("Hope"), cat=Cat("Peace"))
+    return pets[pet]
+
+d = get_pet("dog")
+c = get_pet("cat")
+
+print(d.speak())
+print(c.speak())
