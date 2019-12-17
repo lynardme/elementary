468f578dd66d1f5aea79ab2f123ac571e70f73c2
Lynard <lynardsalingujay@outlook.com>
2019-12-18 01:35:14 +1300

    added cleanup script

--- python-test/cleanup.py
+++ python-test/cleanup.py
@@ -1,9 +1,12 @@
 import os
 import sys
 import time
+import logging
 
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 def remove(path):
+    # check if path is a directory
     if os.path.isdir(path):
         try:
             os.rmdir(path)
@@ -13,29 +16,43 @@ def remove(path):
         try:
             if os.path.exists(path):
                 os.remove(path)
-                print("removed file {}".format(path))
         except OSError:
             print("Unable to remove file: {}".format(path))
 
-def main(past_days, path):
+def main(path=CURRENT_DIR, past_days=14):
+    logging.basicConfig(filename="cleanup.log",
+                        filemode='a',
+                        format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.DEBUG)
+    logging.info("======== Start cleaning up script ========")
+    logging.info("DESC: Delete all files not accessed in the past {} days and any directories that are left empty".format(past_days))
     time_in_secs = time.time() - (past_days * 24 * 60 * 60)
     for root, dirs, files in os.walk(path, topdown=False):
-        # print("root {}".format(root))
-        # print(dirs)
-        # print(files)
         for file in files:
             full_path = os.path.join(root, file)
             stat = os.stat(full_path)
-            print(full_path)
-            print("time {}".format(stat.st_mtime))
-            print("time {}".format(time_in_secs))
 
             if stat.st_mtime <= time_in_secs:
-                # print("removed")
                 remove(full_path)
-    # print(time_in_secs)
-    # print(path)
+                logging.info("Removed file - {}".format(full_path))
+
+        # root is empty
+        if not os.listdir(root):
+            remove(root)
+            logging.info("Removed empty directory - {}".format(root))
+    logging.info("======== End of cleanup script ========")
 
 if __name__ == "__main__":
-    days, path = int(sys.argv[1]), sys.argv[2]
-    main(days, path)
+    if len(sys.argv) == 2:
+        main(sys.argv[1])
+    elif len(sys.argv) == 3:
+        val=None
+        try:
+            val = int(sys.argv[2])
+        except ValueError:
+            print("argument 2 is not a valid int")
+        main(sys.argv[1], val)
+    else:
+        main()
+    print("see cleanup.log...")
